4d7723cdb5ad4b99043c7a4828cea93fac097cea
miasloane <miasloaneellis@gmail.com>
2017-06-14 19:58:22 -0400

    Styling & readme
    Changed styling for navigation to match mock ups & added readme.md file

--- account-settings.php
+++ account-settings.php
@@ -16,17 +16,18 @@ require_once "includes/header.php";
             <!--CONTENT GOES IN HERE: Please use the Materialize grid system!-->
     
             <div class="col s12">
+                <img src="img/humber-logo-webDevPortal.png" id="acctSettForm__logo">
             <form>
                 
                     <div class="col s2">
-                        <p>Img go here</p>
+                        <p>User's image goes here</p>
                     </div>
                   <div class="file-field input-field col s10">
                     <div class="file-path-wrapper">
                       <input class="file-path" type="text" placeholder="Profile Picture">
                     </div>
 
-                    <div class="btn">
+                    <div class="btn" id="acctSettForm__browse">
                       <span>Browse...</span>
                       <input type="file">
                     </div>
@@ -40,12 +41,16 @@ require_once "includes/header.php";
                   <input id="acctSettForm__passField" type="password"placeholder="Password">
                 
                   <input id="acctSettForm__passConfField" type="password" placeholder="Confirm Password">
-                
-                  <input id="acctSettForm__introField" type="text" placeholder="Personal Description (short)">
-                
+                    <div class="input-field">
+                  <input id="acctSettForm__introField" type="text" placeholder="Personal Description (short)" data-length="10">
+                    </div>    
+                    <div class="input-field">
                     <textarea id="acctSettForm__descField" class="materialize-textarea" data-length="120" placeholder="Personal Description (long)"></textarea>
+                </div>
                 
-                <input type="submit" value="Save Changes" class="save">
+                <div class="input-field">
+                <input type="submit" value="Save Changes" class="btn right" id="acctSettForm__save">
+                </div>    
             </form>
             </div>
             
--- includes/loggedin_sidebar.php
+++ includes/loggedin_sidebar.php
@@ -1,5 +1,7 @@
 <ul class="collapsible side-nav fixed bar-color" id="slide-out" data-collapsible="accordion"> <img class="logo" src="img/humber_logo.png" alt="Humber Logo image"> <img class="circle" id="icon" src="img/yuna.jpg" alt="Humber Logo image">
-    <input id="text" type="search" placeholder="Bob Wislon" />
+    <div id="sidebar__searchCont">
+    <input id="search" type="search" placeholder="Search" /><i class="material-icons" id="sidebar__searchIcon">search</i>
+    </div>
     <li>
         <div class="collapsible-header"><i class="material-icons">supervisor_account</i>Account Settings</div>
     </li>
@@ -7,7 +9,7 @@
         <div class="collapsible-header"><i class="material-icons">settings</i>Project Settings</div>
     </li>
     <li>
-        <div class="collapsible-header"><i class="material-icons">note_add</i>Add Projet</div>
+        <div class="collapsible-header"><i class="material-icons">note_add</i>Add Project</div>
     </li>
 </ul>
 <div class="navbar-fixed mobile-only">
@@ -17,7 +19,7 @@
             <ul id="nav-mobile" class="right">
                 <li><a href="#">Account Settings</a></li>
                 <li><a href="#">Project Settings</a></li>
-                <li><a href="#">Add Projet</a></li>
+                <li><a href="#">Add Project</a></li>
             </ul>
         </div>
     </nav>
