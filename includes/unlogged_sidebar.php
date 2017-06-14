42106bb68e8d9925569101efbce5155bdc5ea182
miasloane <miasloaneellis@gmail.com>
2017-06-14 16:13:29 -0400

    Added template page, added notes, added footer.php, removed head from logged_sidebar.php & unlogged_sidebar.php
    note specifies that padding of “main” must be reduced to 0px when
    hamburger appears. Otherwise the sidebar still “takes up space” even
    though it disappears and main content is shifted to the right
    
    Footer.php simply adds closing html tag

--- includes/unlogged_sidebar.php
+++ includes/unlogged_sidebar.php
@@ -1,22 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Humber Student Folio</title>
-    <!--Let browser know website is optimized for mobile-->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-     <!--Import Google Icon Font-->
-    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <!--Custome styles-->
-    <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
-    <link rel="stylesheet" href="css/styles.css">
-    <script>
-        $(".button-collapse").sideNav();
-        
-    </script>
-</head>
-<body>
-    <!--Sidebar-->
+<!--Sidebar-->
            
             <ul class="collapsible side-nav fixed bar-color" id="slide-out" data-collapsible="accordion">
 
@@ -38,10 +20,3 @@
   </ul>
     <a href="#" data-activates="slide-out" class="button-collapse"> <i class="material-icons">menu</i></a>
         <div class="sidebar-footer">	&copy; Humber College 2017</div>
-
-    <!--jquery and materialize js-->
-    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-    <script type="text/javascript" src="js/materialize.min.js"></script>
-</body>
-
-</html>
